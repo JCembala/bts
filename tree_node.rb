7d908742bcf6bc6557710bcfd06646ec8fd0f185
Jakub Cembala <cembala.jakub@gmail.com>
2021-11-17 07:15:06 +0100

    add Comparable module to TreeNode class

--- tree_node.rb
+++ tree_node.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
+# Single tree element
 class TreeNode
+  include Comparable
+  attr_reader :data, :right_children, :left_children
+
+  def initialize(data = nil, left_children = nil, right_children = nil)
+    @data = data
+    @left_children = left_children
+    @right_children = right_children
+  end
+
+  def <=>(other)
+    data <=> other.data
+  end
 end
