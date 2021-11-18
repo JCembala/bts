e8a3dc346d346920e22febde98449fefb9e8560c
Jakub Cembala <cembala.jakub@gmail.com>
2021-11-18 18:42:13 +0100

    modify build_tree method and add helper method to build tree

--- tree.rb
+++ tree.rb
@@ -1,17 +1,34 @@
 # frozen_string_literal: true
 
-require_relative 'tree_node'
+require_relative 'node'
 
 # Tree class allows to build BTS based on array which initializes class
 class Tree
 
   def initialize(array)
-    @root = TreeNode.new
-    @array = array
+    @root = Node.new
+    @array = array.uniq.sort
   end
 
-  # Builds the tree based on given array in #initialize
   def build_tree
-    @root = @array[0]
+    @root = array_to_bts
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  private
+
+  def array_to_bts(arr = @array, start = 0, stop = @array.length)
+    return nil if start > stop
+
+    mid = (start + stop) / 2
+    root = Node.new(arr[mid])
+    root.left = array_to_bts(arr, start, mid - 1)
+    root.right = array_to_bts(arr, mid + 1, stop)
+    root
   end
 end
