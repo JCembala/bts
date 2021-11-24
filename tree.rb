d4ce1260fcae921ab1c6d3ddb0b847542f6ca392
Jakub Cembala <cembala.jakub@gmail.com>
2021-11-24 15:29:57 +0100

    add deletation of node when it's a leaf (no child)

--- tree.rb
+++ tree.rb
@@ -17,6 +17,33 @@ class Tree
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
+  # Deletes a node with given value from tree
+  def delete(node_data)
+    parent = Node.new
+    wanted = @root
+
+    while wanted.data != node_data
+      parent = wanted
+      wanted = wanted.left if node_data < wanted.data
+      wanted = wanted.right if node_data > wanted.data
+    end
+
+    # First case when there is no child
+    # 1. Delete node (remove connection from parent to child)
+    return unless wanted.leaf?
+
+    parent.left = nil if wanted.data < parent.data
+    parent.right = nil if wanted.data > parent.data
+
+    # Second case when there is one child
+    # 1. Find node
+    # 2. Change parent connection from node we are looking to it's child
+    #
+    # Third case when there is more than one child
+    # 1. Search right subtree for next bigger value than removed
+    # 2. Move found value in the place we are removing
+  end
+
   # Inserts a node into tree
   def insert(node_data, root = @root, left_side: true)
     return root.left = Node.new(node_data) if root.left.nil? && left_side
